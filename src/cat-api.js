c4154eb4aba399fe17dcaac407969a05f1f77aeb
o-kovalenko01 <kovalenkoleg777@gmail.com>
2023-11-05 20:43:32 +0200

    updated

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,56 +1,18 @@
-import SlimSelect from 'slim-select';
-import Notiflix from 'notiflix';
 import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_N54J5qpyVgSbvNdXKCr9k0KONlFA7WiP5ogYMjvPQYDiJvnlRwJabi0QU3LpGQvc';
 
-export const fetchBreeds = async () => {
-  const select = new SlimSelect({
-    select: '#breed-select',
+async function fetchBreeds() {
+  const res = await axios.get('https://api.thecatapi.com/v1/breeds');
 
-    events: {
-      afterChange: async newVal => {
-        const loader = document.querySelector('.loader');
-        const info = document.querySelector('.cat-info');
-        // console.log(newVal);
-        if (newVal[0].value === 'Select a cat') {
-          return false;
-        }
-        try {
-          loader.style.display = 'block';
-          info.style.display = 'none';
-          const result = await axios.get(
-            `https://api.thecatapi.com/v1/images/search?breed_ids=${newVal[0].id}`
-          );
-          loader.style.display = 'none';
-          info.style.display = 'block';
+  return res.data;
+}
 
-          info.innerHTML = `<div class = 'image'><img src="${result.data[0].url}" alt=""></div><div><h2>${result.data[0].breeds[0].name}</h2><p>${result.data[0].breeds[0].description}</p><p><b>Temperament: </b>${result.data[0].breeds[0].temperament}</p></div>`;
-        } catch (e) {
-          Notiflix.Notify.failure(
-            ' Oops! Something went wrong! Try reloading the page!'
-          );
-          info.style.display = 'none';
-          loader.style.display = 'none';
-        }
-      },
-    },
-  });
+async function fetchCatByBreed(breedId) {
+  const res = await axios.get(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+  );
+  return res.data[0];
+}
 
-  try {
-    const res = await axios.get('https://api.thecatapi.com/v1/breeds');
-    select.setData([
-      { id: '', text: 'Select a cat' },
-      ...res.data.map(el => {
-        return {
-          text: el.name,
-          id: el.id,
-        };
-      }),
-    ]);
-
-    // console.log(res.data);
-  } catch (e) {
-    Notiflix.Notify.failure(
-      ' Oops! Something went wrong! Try reloading the page!'
-    );
-  }
-};
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,58 @@
 import axios from 'axios';
-import { fetchBreeds } from './cat-api';
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-axios.defaults.headers.common['x-api-key'] =
-  'live_N54J5qpyVgSbvNdXKCr9k0KONlFA7WiP5ogYMjvPQYDiJvnlRwJabi0QU3LpGQvc';
+const loader = document.querySelector('.loader');
+const info = document.querySelector('.cat-info');
+const breed = document.querySelector('.breed-select');
 
-fetchBreeds();
+async function selectCat() {
+  try {
+    breed.style.display = 'none';
+    const res = await fetchBreeds();
+    breed.style.display = 'flex';
+    const select = new SlimSelect({
+      select: '#breed-select',
+
+      events: {
+        afterChange: async newVal => {
+          try {
+            loader.style.display = 'block';
+            info.style.display = 'none';
+
+            const result = await fetchCatByBreed(newVal[0].id);
+
+            loader.style.display = 'none';
+
+            info.style.display = 'block';
+
+            info.innerHTML = `<div class = 'image'><img src="${result.url}" alt=""></div><div><h2>${result.breeds[0].name}</h2><p>${result.breeds[0].description}</p><p><b>Temperament: </b>${result.breeds[0].temperament}</p></div>`;
+          } catch (e) {
+            Notiflix.Notify.failure(
+              ' Oops! Something went wrong! Try reloading the page!'
+            );
+            info.style.display = 'none';
+            loader.style.display = 'none';
+          }
+        },
+      },
+    });
+    select.setData(
+      res.map(el => {
+        return {
+          text: el.name,
+          id: el.id,
+        };
+      })
+    );
+
+    console.log(res);
+  } catch (e) {
+    Notiflix.Notify.failure(
+      ' Oops! Something went wrong! Try reloading the page!'
+    );
+  }
+}
+
+selectCat();
